c89595a05a61e1890bb56af8b60cdfd7cdc13661
ujin16 <94890646+ujin16@users.noreply.github.com>
2023-12-18 03:30:28 +0900

    :sparkles: Feat: 파이어베이스 스토리지 이미지 업로드

--- src/pages/PrintPage.jsx
+++ src/pages/PrintPage.jsx
@@ -1,11 +1,114 @@
 import styled from "styled-components";
 import SnowBg from "../assets/snowBg.svg";
+import html2canvas from "html2canvas";
+import { QRCodeCanvas } from "qrcode.react";
+import { useEffect, useRef, useState } from "react";
+import { storage } from "../firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
-export default function PrintPage() {
+import Spinner from "../assets/Spinner.gif";
+
+export default function PrintPage({ result }) {
+    let idRef = useRef(1);
+    const [imgUrl, setImgUrl] = useState("");
+    const [isQr, setIsQr] = useState(false);
+    const [qrValue, setQrValue] = useState("");
+    const frameType = localStorage.getItem("frameType");
+    const contentRef = useRef(null);
+    const [imagesLoaded, setImagesLoaded] = useState(false);
+
+    const date = new Date();
+    const createdDate = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${("0" + date.getDate()).slice(-2)}`;
+
+    const baseURL = "https://weniv.github.io/photo_booth_of_exhibition/download/";
+
+    // 화면 캡쳐
+    const imageCaptureHandler = async () => {
+        if (!contentRef.current) return;
+        const result = contentRef.current;
+
+        try {
+            const canvas = await html2canvas(result, { scale: 2 });
+            canvas.toBlob((myBlob) => {
+                const myFile = new File([myBlob], `${createdDate}-${idRef.current}.jpeg`, {
+                    type: myBlob && myBlob.type,
+                });
+                handleUpload(myFile);
+            }, "image/jpeg");
+        } catch (error) {
+            console.error("Error converting div to image:", error);
+        }
+    };
+
+    // 이미지 업로드 함수
+    const uploadImage = async (imageFile) => {
+        const storageRef = ref(storage, "images/" + imageFile.name);
+        await uploadBytes(storageRef, imageFile);
+        return getDownloadURL(storageRef);
+    };
+
+    // 이미지 업로드 및 URL 검색
+    const handleUpload = async (imageFile) => {
+        const imageUrl = await uploadImage(imageFile);
+        setImgUrl(imageUrl);
+        setIsQr(true);
+        // console.log("Uploaded Image URL:", imageUrl);
+    };
+
+    useEffect(() => {
+        const images = document.querySelectorAll(".item");
+        let loaded = 0;
+
+        images.forEach((image) => {
+            if (image.complete) {
+                loaded++;
+            } else {
+                image.onload = () => {
+                    loaded++;
+                    if (loaded === images.length) {
+                        setImagesLoaded(true);
+                    }
+                };
+            }
+        });
+
+        if (loaded === images.length) {
+            setImagesLoaded(true);
+        }
+    }, []);
+
+    useEffect(() => {
+        setIsQr(false);
+
+        if (imagesLoaded) {
+            // 모든 이미지가 로드된 후 html2canvas 호출
+            imageCaptureHandler();
+        }
+    }, [imagesLoaded]);
+
+    useEffect(() => {
+        if (imgUrl.length > 1) {
+            const specificUrl = imgUrl.split("images")[1];
+            setQrValue(baseURL + specificUrl);
+        }
+    }, [imgUrl]);
+
     return (
         <Wrap>
-            <img src={`${process.env.PUBLIC_URL}/images/type1.svg`} style={{ background: "#D9D9D9" }} />
-            <QrBox />
+            <ImgBox ref={contentRef}>
+                <FlexBox>
+                    {result.map((item, idx) => (
+                        <div key={idx}>
+                            <img className="item" id={idx} src={item} />
+                        </div>
+                    ))}
+                </FlexBox>
+                <Frame src={process.env.PUBLIC_URL + `/images/${frameType}.svg`} alt="" />
+            </ImgBox>
+            <QrBox>
+                {console.log(qrValue)}
+                <LoadingBox>{isQr && qrValue ? <QRCodeCanvas value={qrValue} size={53} /> : <img src={Spinner} style={{ width: "60px" }} alt="로딩중" />}</LoadingBox>
+            </QrBox>
             <Message>QR코드를 통해 사진을 다운로드 하세요</Message>
         </Wrap>
     );
@@ -20,14 +123,48 @@ const Wrap = styled.main`
     display: flex;
     flex-direction: column;
     align-items: center;
+`;
 
-    img {
-        margin-top: 178px;
-        width: 570px;
+const ImgBox = styled.div`
+    position: relative;
+    margin-top: 178px;
+    width: 570px;
+    height: 855px;
+    background: #d9d9d9;
+    overflow: hidden;
+`;
+
+const FlexBox = styled.div`
+    margin: 109px 31px;
+    display: flex;
+    gap: 11px;
+    flex-wrap: wrap;
+
+    div {
+        position: relative;
+        width: 248px;
+        height: 312px;
+        overflow: hidden;
+
+        img {
+            transform: scaleX(-1);
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100%;
+        }
     }
 `;
 
-const Image = styled.img``;
+const Frame = styled.img`
+    width: 570px;
+    z-index: 100;
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+`;
 
 const QrBox = styled.div`
     width: 82px;
@@ -35,6 +172,16 @@ const QrBox = styled.div`
     border-radius: 5px;
     background: #a82626;
     margin: 71px 0 45px;
+    padding: 7px;
+`;
+
+const LoadingBox = styled.div`
+    background: #fff;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 3.375px;
 `;
 
 const Message = styled.p`
